4f9493499509b49b9020a4e7370a0edf2b5bfb03
Max-IT-ZT <babuskomax@gmail.com>
2023-07-01 00:55:42 +0300

    fix-animate

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,31 @@
 function smoothScrollTo(elementId) {
   const element = document.getElementById(elementId);
-  element.scrollIntoView({ behavior: 'smooth' });
+  const targetPosition =
+    element.getBoundingClientRect().top + window.pageYOffset;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1000; // Задайте бажану тривалість прокрутки
+
+  let startTime = null;
+
+  function scrollAnimation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+
+    const timeElapsed = currentTime - startTime;
+    const scrollStep = Math.round((distance * timeElapsed) / duration);
+
+    window.scrollTo(0, startPosition + scrollStep);
+
+    if (timeElapsed < duration) {
+      requestAnimationFrame(scrollAnimation);
+    } else {
+      window.scrollTo(0, targetPosition);
+    }
+  }
+
+  requestAnimationFrame(scrollAnimation);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,3 +40,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > document.documentElement.scrollHeight * 0.15 ||
+    document.documentElement.scrollTop >
+      document.documentElement.scrollHeight * 0.15
+  ) {
+    document.getElementById('scrollUpBtn').style.display = 'flex';
+  } else {
+    document.getElementById('scrollUpBtn').style.display = 'none';
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
